a7d3938f21fc989e90cc68f33f7240c92cfe4400
GolamSaroarGitHub <golamsaroar89@gmail.com>
2016-10-07 16:29:00 +0600

    The problem solved for not printing in the timer text on web page.... but still need some more fixed like the toggle button and the Reset

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-var timer1=document.getElementById('timer');
-var toggeButton=document.getElementById('toggle');
-var resetButton=document.getElementById('reset');
+var timer = document.getElementById('timer');
+var toggeButton = document.getElementById('toggle');
+var resetButton = document.getElementById('reset');
 
-var watch=new Stopwatch(timer1);
-toggeButton.addEventListener('click',function(){
-  if (watch.isOn) {
-    watch.stop();
+var watch = new Stopwatch(timer);
+toggeButton.addEventListener('click', function() {
+	if (watch.isOn) {
+		watch.stop();
 
-  }
-  else {
-    watch.start();
-  }
+	} else {
+		watch.start();
+	}
 });
 
-resetButton.addEventListener('click',function() {
-  watch.reset();
+resetButton.addEventListener('click', function() {
+	watch.reset();
 });
 console.log('hi');
--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,65 +1,67 @@
 function Stopwatch(elem) {
+	// function Stopwatch() {
+	var time = 0;
+	var interval;
+	var offset;
 
-var time=0;
-var interval;
-var offset;
-function update() {
-time+=delta();
+	function update() {
+		time += delta();
 
-var formattedTime=timeFormatter(time);
-elem.innerHTML=formattedTime.toString();
-// console.log(formattedTime);
+		//var formattedTime=timeFormatter(time);
+		var formattedTime = timeFormatter(time);
+		elem.innerHTML = formattedTime.toString();
+		console.log(elem);
+	}
+
+	function delta() {
+		var now = Date.now();
+		var timePassed = now - offset;
+		offset = now;
+		return timePassed;
+	}
+
+	function timeFormatter(timeInMiliseconds) {
+
+		var time = new Date(timeInMiliseconds);
+		var minutes = time.getMinutes().toString();
+		var seconds = time.getSeconds().toString();
+		var milliseconds = time.getMilliseconds().toString();
+
+		if (minutes.length < 2)
+			minutes = '0' + minutes;
+		if (seconds.length < 2)
+			seconds = '0' + seconds;
+
+		while (milliseconds.length < 3)
+			milliseconds = '0' + milliseconds;
+
+		return minutes + ' : ' + seconds + '. ' + milliseconds;
+	}
+
+
+	this.isOn = false;
+	this.start = function() {
+		if (!this.isOn) {
+			this.isOn = true;
+			interval = setInterval(update, 10);
+			offset = Date.now();
+
+
+		}
+
+
+	};
+	this.stop = function() {
+		if (this.isOn) {
+			this.isOn = false;
+			clearInterval(interval);
+			interval = null;
+		}
+	};
+	this.reset = function() {
+		time = 0;
+
+	};
+
 }
-function delta() {
-var now =Date.now();
-var timePassed=now-offset;
-  offset=now;
-  return timePassed;
-}
-function timeFormatter(timeInMiliseconds) {
-
-  var time=new Date(timeInMiliseconds);
-  var minutes=time.getMinutes().toString();
-  var seconds=time.getSeconds().toString();
-  var milliseconds=time.getMilliseconds().toString();
-
-  if(minutes.length<2)
-  minutes='0'+minutes;
-   if(seconds.length<2)
-   seconds='0'+seconds;
-
-   while(milliseconds.length<3)
-   milliseconds='0'+milliseconds;
-
-   return minutes+' : '+seconds+'. '+milliseconds;
-}
-
-
-this.isOn=false;
-this.start=function(){
- if(!this.isOn)
- {
-  this.isOn=true;
-  interval=setInterval(update, 10);
-  offset=Date.now();
-
-
- }
-
-
-  };
-  this.stop=function () {
-    if(this.isOn)
-    {
-      this.isOn=false;
-      clearInterval(interval);
-      interval=null;
-    }
-  };
-  this.reset=function () {
-    time=0;
-
-  };
-
-}
-//var watch=new Stopwatch();
+// var watch = new Stopwatch();
